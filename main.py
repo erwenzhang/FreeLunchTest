9c9e097afb76c754c133feac6daeb33a9498f764
xiangkundai <xiangkundai@gmail.com>
2015-12-04 17:11:59 -0600

    clean

--- main.py
+++ main.py
@@ -1,44 +1,25 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
 import cgi
 import urllib
-
 from google.appengine.api import users
 from google.appengine.ext import ndb
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.api import images
 
-
 administrator1 = users.User("wenwenzhang1001@gmail.com")
 # administrator2 = users.User("")
 
 class Event(ndb.Model):
-    name = ndb.StringProperty(required= True)
+    name = ndb.StringProperty(required=True)
     description = ndb.StringProperty()
-    coverurl=ndb.StringProperty()
-
+    coverurl = ndb.StringProperty()
     authorID = ndb.StringProperty(default="administrator")
     author_name = ndb.StringProperty(default="administrator")
-
     loc = ndb.GeoPtProperty(required=True,default=ndb.GeoPt(0,0))
     date = ndb.DateTimeProperty(required=True)
-
     linkage = ndb.StringProperty()
 
 class Crowdworker(ndb.Model):
@@ -47,11 +28,13 @@ class Crowdworker(ndb.Model):
     rated_times = ndb.IntegerProperty()
     score = ndb.IntegerProperty()
 
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+
+class AddEvent(webapp2.RequestHandler):
+    def
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)#,
