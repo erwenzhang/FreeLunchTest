357b691077efb9803de21a0becff30e9defabd8b
xiangkundai <xiangkundai@gmail.com>
2015-12-04 20:35:02 -0600

    tested add

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import webapp2
 import cgi
 import urllib
 import json
+import datetime
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -67,8 +68,8 @@ class ViewAllEvents(webapp2.RequestHandler):
         names = []
 
         for event in events:
-            locations.append(event.loc)
-            dates.append(event.date)
+            locations.append(str(event.loc))
+            dates.append(str(event.date))
             names.append(event.name)
 
         dictPassed = {'dates':dates, 'names':names,'locations':locations}
@@ -91,8 +92,8 @@ class ViewOneEvent(webapp2.RequestHandler):
             ratings = None
             author_name = None
 
-        dictPassed = {'date':the_event.date,
-                      'location':the_event.loc,
+        dictPassed = {'date':str(the_event.date),
+                      'location':str(the_event.loc),
                       'description':the_event.description,
                       'coverUrl':the_event.coverUrl,
                       'linkage':the_event.linkage,
@@ -115,18 +116,25 @@ class GiveFeedback(webapp2.RequestHandler):
         author.put()
 
 
-
+class AddEvent(webapp2.RequestHandler):
+    def get(self):
+        event = Event()
+        event.name = 'Test Event'
+        event.description = 'Test description'
+        event.date = datetime.datetime.now()
+        event.put()
+        self.redirect('/')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     # ('/Mapview',MapView),
     # ('/Calendarview',CalendarView),
-    # ('/Addevent',AddEvent),
+    ('/Addevent',AddEvent),
     ('/ViewOneEvent',ViewOneEvent),
     ('/ViewAllEvents',ViewAllEvents),
     # ('/ViewOneWorker',ViewOneWorker),
     # ('/ViewAllWorkers',ViewAllWorkers),
-    ('/GiveFeedback',GiveFeedback),
+    ('/GiveFeedback',GiveFeedback)#,
     # ('/DeleteEvent',DeleteEvent)
 ], debug=True)
 
